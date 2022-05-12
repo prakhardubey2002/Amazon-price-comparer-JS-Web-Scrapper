039141ccd9cef661bce8d7a37d950db7c9c1b059
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-05-12 23:50:25 +0530

    code formatted

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const { parse } = require('dotenv');
-
 const nightmare = require('nightmare')()//this is returning us a function that we can use it by addind () at end
 
 const readline = require("readline");//built-in node library for input and output interface and management
@@ -14,13 +12,11 @@ r2.question("Enter name\n", function (string) {
     r2.close();
 });
 
-    checkprice()
 
+checkprice()
 
 //https://www.amazon.in/Samsung-Internal-Solid-State-MZ-V7S500BW/dp/B07MFBLN7K
-
 async function checkprice() {
-   
     const priceString = await nightmare.goto("https://www.amazon.in/Samsung-Internal-Solid-State-MZ-V7S500BW/dp/B07MFBLN7K")
         .wait("#mbc-price-1")
         .evaluate(() => document.getElementById("mbc-price-1").innerText)//amazon price class and id are subject to change with time id available here may fail to retrieve data in future because of its replacement with newer class or id
@@ -30,15 +26,15 @@ async function checkprice() {
     const price = (priceNumber.replace('₹', ''))
     // console.log(price);
     const a = Math.trunc(price)
-    console.log(a);
-   
-   
+    console.log(`Your item  Samsung evo with current price of ${a}`);
+
+
     if (a < 6500) {
         console.log(` ${name} your item  is Cheap now`);
     }
     if
         (a > 6500) {
-        console.log(`${name} your item is costly now`);
+        console.log(`${name} your item is costly now `);
     }
     else {
         console.error("unable resolve request");
