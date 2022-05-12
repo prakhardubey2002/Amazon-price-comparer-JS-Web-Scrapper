6833f7c41b78aecabf26588dbfaf6ae095d7528a
Prakhar Dubey <prakhardubey2002@gmail.com>
2022-05-12 16:55:13 +0530

    scrapping link set,Readline intialised

--- parser.js
+++ parser.js
@@ -1,10 +1,46 @@
+const { parse } = require('dotenv');
+
 const nightmare = require('nightmare')()//this is returning us a function that we can use it by addind () at end
 
+const readline = require("readline");//built-in node library for input and output interface and management
 
-async function checkprice(){
-    const priceString = await   nightmare.goto("https://www.amazon.in/Samsung-Internal-Solid-State-MZ-V7S500BW/dp/B07MFBLN7K")
-                                .wait(".a-price-whole")
-                                .evaluate(()=>document.getElementsByClassName("a-price-whole").innerText)
-                                .end()
+const r2 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+let name = "";
+r2.question("Enter name\n", function (string) {
+    name = string;
+    r2.close();
+});
 
+    checkprice()
+
+
+//https://www.amazon.in/Samsung-Internal-Solid-State-MZ-V7S500BW/dp/B07MFBLN7K
+
+async function checkprice() {
+   
+    const priceString = await nightmare.goto("https://www.amazon.in/Samsung-Internal-Solid-State-MZ-V7S500BW/dp/B07MFBLN7K")
+        .wait("#mbc-price-1")
+        .evaluate(() => document.getElementById("mbc-price-1").innerText)//amazon price class and id are subject to change with time id available here may fail to retrieve data in future because of its replacement with newer class or id
+        .end()
+
+    const priceNumber = (priceString.replace(',', ''))
+    const price = (priceNumber.replace('₹', ''))
+    // console.log(price);
+    const a = Math.trunc(price)
+    console.log(a);
+   
+   
+    if (a < 6500) {
+        console.log(` ${name} your item  is Cheap now`);
+    }
+    if
+        (a > 6500) {
+        console.log(`${name} your item is costly now`);
+    }
+    else {
+        console.error("unable resolve request");
+    }
 }
